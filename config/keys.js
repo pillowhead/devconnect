b1c91eb7ca29aff4d318ed8fe967fadf98d73103
pillowhead <gabriel.belmonte@gmail.com>
2018-11-22 16:32:51 +0100

    partial creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin1:admin1@ds231529.mlab.com:31529/devconnector"
+  mongoURI: "mongodb://admin1:admin1@ds231529.mlab.com:31529/devconnector",
+  secretOrKey: "secret"
 };
