e9a7f490800f88849b6c57826b32f48216e2436d
pillowhead <gabriel.belmonte@gmail.com>
2018-11-23 14:51:51 +0100

    Passport JWT Authentication Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // Load User model
 const User = require("../../models/User");
@@ -87,5 +88,22 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @router  GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //res.json({ msg: "Success" }); Test to see msg Success
+    //res.json(req.user); Test to see user details sent to req
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
